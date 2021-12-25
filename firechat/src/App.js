d3674693d536c2392f55366efd1d14460b6b6a34
tejaswilkhoo <tejaswilkhoo@hotmail.com>
2021-12-25 17:49:26 -0500

    basic chat complete without production build

--- firechat/src/App.js
+++ firechat/src/App.js
@@ -62,28 +62,59 @@ function SignOut() {
 }
 
 function ChatRoom() {
+
+    const dummy=useRef();
+
     const messagesRef = firestore.collection('messages');
     const query = messagesRef.orderBy('createdAt').limit(25);
-
     const [messages] = useCollectionData(query, {idField: 'id'});
+    const [formValue, setFormValue] = useState('');
+
+    const sendMessage = async(e) => {
+        e.preventDefault();
+
+        const { uid, photoURL } = auth.currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        });
+
+        setFormValue('');
+
+        dummy.current.scrollIntoView({ behavior: 'smooth' })
+    }
+
 
     return (
         <>
-            <div>
+            <main>
                 {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
-            </div>
-            <div>
+            
+                <div ref={dummy}></div>
+            </main>
 
-            </div>
+            <form onSubmit={sendMessage}>
+                <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+                <button type='submit'>Send</button>
+            </form>
         </>
     )
 }
 
 function ChatMessage(props) {
-    const { text, uid } = props.message;
+    const { text, uid, photoURL } = props.message;
     
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';    
 
-    return <p>{text}</p>
+    return (
+        <div className={`message ${messageClass}`}>
+            <img src={photoURL} />
+            <p>{text}</p>
+        </div>
+    )
 }
 
 
